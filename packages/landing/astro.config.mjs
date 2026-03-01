81879bf83fc0f75a8620b1b9e99e3adce722dc37
Oskar Freye <oskar@freye.tech>
2026-03-01 20:28:57 +0100

    fix(landing): add astro-sst adapter for SST deployment

--- packages/landing/astro.config.mjs
+++ packages/landing/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import aws from "astro-sst";
 
 export default defineConfig({
 	site: "https://draht.dev",
 	outDir: "./dist",
+	adapter: aws(),
 	integrations: [sitemap()],
 });

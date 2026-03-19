df57aa4babf5ae09347f60c1f14fcf623b73cf03
Mario Zechner <badlogicgames@gmail.com>
2026-03-19 22:21:48 +0100

    fix(coding-agent): suppress process warnings in CLI closes #2404

--- packages/coding-agent/src/cli.ts
+++ packages/coding-agent/src/cli.ts
@@ -5,7 +5,7 @@
  *
  * Test with: npx tsx src/cli-new.ts [args...]
  */
-process.title = "draht";
+process.title = "pi";
 process.emitWarning = (() => {}) as typeof process.emitWarning;
 
 import { EnvHttpProxyAgent, setGlobalDispatcher } from "undici";
